cf8fe9d87642074d524660c57df634be3982740a
Sahaja Lal <james@conduit.vc>
2018-05-18 15:40:30 -0600

    Add script for creating cognito users

---
+++ packages/aws-utils/bin/createCognitoUser.js
@@ -0,0 +1,100 @@
+#! /usr/bin/env node
+
+// XXX: Hack to ensure we get the test config.
+process.env.NODE_ENV = 'test';
+// needed for amazon-cognito-identity-js
+global.fetch = require('node-fetch');
+
+const uuid = require('uuid/v4');
+const aws = require('aws-sdk');
+const createConfig = require('@conduitvc/config');
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+
+const config = createConfig(module.parent);
+const userPool = new AmazonCognitoIdentity.CognitoUserPool({
+  UserPoolId: config.userPool.id,
+  ClientId: config.userPool.clientId,
+  region: config.cognito.region,
+});
+
+const createUser = async (cognito, { userPoolId, username, password }) => {
+  const tempPassword = uuid();
+  await cognito
+    .adminCreateUser({
+      UserPoolId: userPoolId,
+      Username: username,
+      MessageAction: 'SUPPRESS',
+      TemporaryPassword: tempPassword,
+      UserAttributes: [
+        { Name: 'email_verified', Value: 'True' },
+        { Name: 'email', Value: 'user@example.com' },
+      ],
+    })
+    .promise();
+
+  const authDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+    Username: username,
+    Password: tempPassword,
+  });
+  const user = new AmazonCognitoIdentity.CognitoUser({
+    Username: username,
+    Pool: userPool,
+  });
+  user.setAuthenticationFlowType('USER_SRP_AUTH');
+
+  await new Promise((accept, reject) => {
+    user.authenticateUser(authDetails, {
+      onSuccess: () => {
+        accept(user);
+      },
+      onFailure: (err) => {
+        reject(err);
+      },
+      newPasswordRequired: () => {
+        accept(user);
+      },
+    });
+  });
+
+  await new Promise((accept, reject) => {
+    user.completeNewPasswordChallenge(
+      password,
+      {},
+      {
+        onSuccess: accept,
+        onFailure: reject,
+      },
+    );
+  });
+
+  return user;
+};
+
+async function main() {
+  const username = uuid();
+  const password = uuid();
+  const cognito = new aws.CognitoIdentityServiceProvider(config.cognito);
+  const user = await createUser(cognito, {
+    userPoolId: config.userPool.id,
+    username,
+    password,
+  });
+
+  const idKey = user
+    .getSignInUserSession()
+    .getIdToken()
+    .getJwtToken();
+
+  console.log('Created Cognito user for testing:');
+  console.log();
+  console.log('  Username: ', username);
+  console.log('  Password: ', password);
+  console.log('  JWT ID Token:');
+  console.log(idKey);
+  console.log();
+}
+
+main().catch((err) => {
+  console.error('Something went wrong', err.stack);
+  process.exit(1);
+});
---
+++ packages/aws-utils/findServerless.js
@@ -0,0 +1,28 @@
+const GlobalCache = {};
+const yaml = require('js-yaml');
+const path = require('path');
+const pkgUp = require('pkg-up');
+const fs = require('fs');
+
+const findServerlessPath = ({ parent: { filename } } = module) =>
+  path.dirname(pkgUp.sync(filename));
+
+const findServerless = (mod = module) => {
+  const serverlessPath = path.join(findServerlessPath(mod), 'serverless.yml');
+  if (GlobalCache[serverlessPath]) {
+    return GlobalCache[serverlessPath];
+  }
+
+  if (!fs.existsSync(serverlessPath)) {
+    throw new Error(`Expected serverless file at location: ${serverlessPath}`);
+  }
+
+  const parsed = yaml.safeLoad(fs.readFileSync(serverlessPath, 'utf8'));
+  // eslint-disable-next-lin
+  return (GlobalCache[serverlessPath] = parsed);
+};
+
+module.exports = {
+  findServerlessPath,
+  findServerless,
+};
