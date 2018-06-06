271729a2ad855ffcb2f11130f9e9d79f72ce9a65
Christopher Baron <topherbaron@gmail.com>
2018-06-06 12:13:19 -0400

    createServless is syntactically sound for evaluation, saving work

--- packages/aws-utils/bin/createServerless.js
+++ packages/aws-utils/bin/createServerless.js
@@ -5,31 +5,21 @@ const YAML = require('yamljs');
 const createConfig = require('@conduitvc/config');
 const config = createConfig(module.parent);
 
+const capitalize = val => val.charAt(0).toUpperCase() + val.slice(1);
 const newline = i => [...Array(i).keys()].reduce((sum,i) => sum + '\n','');
 
 const provider = {
-  provider:
+  provider: {
     name: 'aws',
     runtime: 'nodejs6.10',
     stage: 'dev',
     region: config.region,
+  }
 }
 
 const plugins = [
   'serverless-appsync-plugin'
 ];
-
-const custom = {
-  accountId: config.accountId,
-  appSync: {
-    name: config.name,
-    apiId: config.apiId || undefined,
-    authenticationType: 'AMAZON_COGNITO_USER_POOLS',
-    userPoolConfig: config.userPool,
-    region: config.region,
-    mappingTemplates: deriveMappingTemplates(config)
-  }
-}
 
 const deriveMappingTemplates = config => {
   return config.entities.reduce((sum,entity) => {
@@ -42,7 +32,7 @@ const deriveMappingTemplates = config => {
     },{
       dataSource: entity.name,
       type: 'Query',
-      field: entity.name',
+      field: entity.name,
       request: "byId-request.txt",
       response: "resultItems-response.txt",
     },{
@@ -64,15 +54,138 @@ const deriveMappingTemplates = config => {
       request: "delete-request.txt",
       response: "result-response.txt",
     }])
-    if(entity.subscriptions)
+    if(entity.subscriptions) {
+      entity.subscriptions.forEach(subscription => {
+        sum.push({
+          dataSource: 'SubscriberPassthrough',
+          type: 'Subscription',
+          field: 'subscribeTo' + capitalize(subscription) + entity.name,
+          request: "subscribePassthrough-request.txt",
+          response: "subscribePassthrough-response.txt",
+        })
+      })
+    }
     return sum
   }, [])
+}
+
+const deriveDataSources = config => {
+  const dataSources = config.entities.map(entity => ({
+    type: 'AMAZON_DYNAMODB',
+    name: entity.name,
+    description: entity.description,
+    config: {
+      tableName: entity.name,
+      serviceRoleArn: "arn:aws:iam::${self:custom.accountId}:role/Dynamo-${self:custom.appSync.serviceRole}",
+    },
+  }));
+
+  if( config.entities.some(entity => entity.subscriptions) ) {
+    dataSources.push({
+      type: 'None',
+      name: 'SubscriberPassthrough',
+      description: 'Non-datasource datasource',
+      config: {
+        serviceRoleArn: "arn:aws:iam::${self:custom.accountId}:role/Dynamo-${self:custom.appSync.serviceRole}"
+      }
+    })
+  }
+  return dataSources;
+}
+
+const custom = {
+  accountId: config.accountId,
+  appSync: {
+    accountId: "${env:AWS_ACCOUNT_ID}",
+    name: config.name,
+    apiId: config.apiId || undefined,
+    authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+    userPoolConfig: {
+      ...config.userPool,
+      userPoolId: "${env:USER_POOL_ID}"
+    },
+    region: config.region,
+    mappingTemplates: deriveMappingTemplates(config),
+    serviceRole: "AppSyncServiceRole",
+    dataSources: deriveDataSources(config),
+  }
+}
+
+const deriveResources = config => {
+  const resources = {
+    Resources: config.entities.reduce((sum,entity) => {
+      return {
+        ...sum,
+        [entity.name + 'Table']: {
+          Type: "AWS::DynamoDB::Table",
+          Properties: {
+            KeySchema: [{
+              AttributeName: 'id',
+              KeyType: 'HASH',
+            }],
+            AttributeDefinitions: [{
+              AttributeName: 'id',
+              AttributeType: 'S',
+            }],
+            ProvisionedThroughput: {
+              ReadCapacityUnits: 10,
+              WriteCapacityUnits: 10,
+            },
+            TableName: entity.name,
+          },
+        },
+      }
+    }, { } ),
+  };
+  
+  resources.AppSyncServiceRole = {
+    Type: "AWS::IAM::Role",
+    Properties: {
+      RoleName: "Dynamo-AppSyncServiceRole",
+      AssumeRolePolicyDocument: {
+        Version: "2012-10-17",
+        Statement: [{
+          Effect: "Allow",
+          Principal: {
+            Service: [ "appsync.amazonaws.com" ],
+          },
+          Action: [ "sts:AssumeRole" ],
+        }],
+      },
+      Policies: [{
+        PolicyName: "Dynamo-AppSyncServiceRole-Policy",
+        PolicyDocument: {
+          Version: "2012-10-17",
+          Statement: [{
+            Effect: "Allow",
+            Action: [
+              "dynamodb:Query",
+              "dynamodb:BatchWriteItem",
+              "dynamodb:GetItem",
+              "dynamodb:DeleteItem",
+              "dynamodb:PutItem",
+              "dynamodb:Scan",
+              "dynamodb:UpdateItem",
+            ],
+            Resource: config.entities.reduce((sum,entity) =>
+              [
+                ...sum,
+                `arn:aws:dynamodb:${config.region}:*:table/${entity.name}`,
+                `arn:aws:dynamodb:${config.region}:*:table/${entity.name}/*`,
+              ], [] )
+          }],
+        },
+      }],
+    },
+  }
+
+  return resources;
 }
 
 async function main() {
   let input = process.argv[2];
 
-  if(! (await fs.exists(input)) ) {
+  if(!input || !(await fs.exists(input))) {
     input = process.cwd();
   }
 
@@ -82,158 +195,14 @@ async function main() {
   serverlessYml.write(`frameworkVersion: ">=1.21.0 <2.0.0"` + newline(2));
   serverlessYml.write(YAML.stringify(provider, 2) + newline(2))
   serverlessYml.write(YAML.stringify(plugins, 2) + newline(2));
-  serverlessYml.end(YAML.stringify(custom, 2));
+  serverlessYml.write(YAML.stringify(custom, 2) + newline(1));
+  serverlessYml.end(YAML.stringify(deriveResources(config), 2) + newline(1));
+
+  console.log('(⌐■_■) Great Job! (⌐■_■)');
+  process.exit(0);
 }
 
 main().catch((err) => {
   console.error('Something went wrong', err.stack);
   process.exit(1);
 });
-
-service: serverless-appsync-subscription-test
-
-frameworkVersion: ">=1.21.0 <2.0.0"
-
-provider:
-  name: aws
-  runtime: nodejs6.10
-  stage: dev
-  region: us-east-2
-
-plugins:
-  - serverless-appsync-plugin
-
-custom:
-  accountId: 229055845648 # replace this with your accountId
-  appSync:
-    name: quote-provider
-    apiId: guvvxnz5xzccfan6dttukk6hni
-    authenticationType: AMAZON_COGNITO_USER_POOLS
-    userPoolConfig:
-      awsRegion: us-east-2 # required # region
-      defaultAction: ALLOW
-      userPoolId: us-east-2_PKa2Qpncv # replace this with your Cognito User Pool Id
-    # region: # defaults to provider region
-    # mappingTemplatesLocation: # defaults to mapping-templates
-    mappingTemplates:
-      - dataSource: QuoteRequest
-        type: Query
-        field: quoteRequests
-        request: "quoteRequests-request.txt"
-        response: "list-response.txt"
-      - dataSource: QuoteResponse
-        type: QuoteRequest
-        field: quoteResponse
-        request: "quoteResponse-request.txt"
-        response: "quoteResponse-response.txt"
-      - dataSource: QuoteRequest
-        type: Mutation
-        field: requestQuote
-        request: "requestQuote-request-mapping-template.txt"
-        response: "requestQuote-response-mapping-template.txt"
-      - dataSource: QuoteResponse
-        type: Mutation
-        field: respondToQuote
-        request: "respondToQuote-request-mapping-template.txt"
-        response: "respondToQuote-response-mapping-template.txt"
-      - dataSource: QuoteRequest
-        type: CreatedQuoteResponse
-        field: respondedToQuote
-        request: "respondedToQuote-request.txt"
-        response: "respondedToQuote-response.txt"
-      - dataSource: SubscriberPassthrough
-        type: Subscription
-        field: subscribeToQuoteRequest
-        request: "subscribeToQuoteRequest-request-mapping-template.txt"
-        response: "subscribeToQuoteRequest-response-mapping-template.txt"
-      - dataSource: SubscriberPassthrough
-        type: Subscription
-        field: subscribeToQuoteResponse
-        request: "subscribeToQuoteResponse-request.txt"
-        response: "subscribeToQuoteResponse-response.txt" 
-    serviceRole: "AppSyncServiceRole"
-    dataSources:
-      - type: AMAZON_DYNAMODB
-        name: QuoteRequest
-        description: 'Each item contains a customer quote request'
-        config:
-          tableName: 'QuoteRequest'
-          serviceRoleArn: "arn:aws:iam::${self:custom.accountId}:role/Dynamo-${self:custom.appSync.serviceRole}"
-      - type: AMAZON_DYNAMODB
-        name: QuoteResponse
-        description: 'Each item contains a response to a customer quote request'
-        config:
-          tableName: 'QuoteResponse'
-          serviceRoleArn: "arn:aws:iam::${self:custom.accountId}:role/Dynamo-${self:custom.appSync.serviceRole}"
-      - type: NONE
-        name: SubscriberPassthrough
-        description: 'Non-datasource datasource'
-        config:
-          serviceRoleArn: "arn:aws:iam::${self:custom.accountId}:role/Dynamo-${self:custom.appSync.serviceRole}"
-resources:
-  Resources:
-    QuoteRequestTable:
-      Type: "AWS::DynamoDB::Table"
-      Properties:
-        KeySchema:
-          -
-            AttributeName: id
-            KeyType: HASH
-        AttributeDefinitions:
-          -
-            AttributeName: id
-            AttributeType: S
-        ProvisionedThroughput:
-          ReadCapacityUnits: 10
-          WriteCapacityUnits: 10
-        TableName: "QuoteRequest"
-    QuoteResponse:
-      Type: "AWS::DynamoDB::Table"
-      Properties:
-        KeySchema:
-          -
-            AttributeName: id
-            KeyType: HASH
-        AttributeDefinitions:
-          -
-            AttributeName: id
-            AttributeType: S
-        ProvisionedThroughput:
-          ReadCapacityUnits: 10
-          WriteCapacityUnits: 10
-        TableName: "QuoteResponse"
-    AppSyncServiceRole:
-      Type: "AWS::IAM::Role"
-      Properties:
-        RoleName: "Dynamo-AppSyncServiceRole"
-        AssumeRolePolicyDocument:
-          Version: "2012-10-17"
-          Statement:
-            -
-              Effect: "Allow"
-              Principal:
-                Service:
-                  - "appsync.amazonaws.com"
-              Action:
-                - "sts:AssumeRole"
-        Policies:
-          -
-            PolicyName: "Dynamo-AppSyncServiceRole-Policy"
-            PolicyDocument:
-              Version: "2012-10-17"
-              Statement:
-                -
-                  Effect: "Allow"
-                  Action:
-                    - "dynamodb:Query"
-                    - "dynamodb:BatchWriteItem"
-                    - "dynamodb:GetItem"
-                    - "dynamodb:DeleteItem"
-                    - "dynamodb:PutItem"
-                    - "dynamodb:Scan"
-                    - "dynamodb:UpdateItem"
-                  Resource:
-                    - "arn:aws:dynamodb:us-east-2:*:table/QuoteRequest"
-                    - "arn:aws:dynamodb:us-east-2:*:table/QuoteRequest/*"
-                    - "arn:aws:dynamodb:us-east-2:*:table/QuoteResponse"
-                    - "arn:aws:dynamodb:us-east-2:*:table/QuoteResponse/*"
