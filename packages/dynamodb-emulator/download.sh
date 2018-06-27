4f91e6c357bf9dad4348b3ae3c6a27975ec01cb5
Sahaja Lal <james@conduit.vc>
2018-06-27 14:15:09 -0600

    Patch downloaded emulator for more logs

---
+++ packages/dynamodb-emulator/download.sh
@@ -0,0 +1,26 @@
+#! /bin/bash -ex
+
+download --extract -o emulator https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.zip
+
+pushd emulator
+cat > log4j2.xml <<- XML
+<?xml version="1.0" encoding="UTF-8"?>
+<Configuration status="fatal">
+  <Appenders>
+    <Console name="STDOUT" target="SYSTEM_OUT">
+      <PatternLayout pattern="%d %-5p [%t] %C{2} (%F:%L) - %m%n"/>
+    </Console>
+    </Appenders>
+  <Loggers>
+    <Logger name="com.amazonaws.services.dynamodbv2.local" level="debug" />
+    <Root level="debug">
+      <AppenderRef ref="STDOUT"/>
+    </Root>
+  </Loggers>
+</Configuration>
+XML
+
+zip -d DynamoDBLocal.jar log4j2.xml
+zip -u DynamoDBLocal.jar log4j2.xml
+rm log4j2.xml
+popd emulator
