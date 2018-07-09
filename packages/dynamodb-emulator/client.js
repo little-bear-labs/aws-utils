46e59d373ee80ad901add2aaad36c46e986a3830
Christopher Baron <topherbaron@gmail.com>
2018-07-09 09:21:49 -0400

    creating deamon pidPath if it does not exist

--- packages/dynamodb-emulator/client.js
+++ packages/dynamodb-emulator/client.js
@@ -87,6 +87,9 @@ async function requestEmulator(unixSocketFile, options) {
 }
 
 async function startDeamon(hash, unixSocketFile, options) {
+  if(!fs.existsSync(pidPath)) {
+    fs.mkdirSync(pidPath);
+  }
   // file does not exist so we will need to create one.
   const pidFile = path.join(pidPath, `${hash}.pid`);
   const fd = fs.openSync(pidFile, 'w');
