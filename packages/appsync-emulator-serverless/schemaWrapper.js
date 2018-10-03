bbfd93cffb3e6603114820eda604b0c994c2ff31
Samuel Oltz <soltz@rapiddg.com>
2018-10-03 10:26:50 -0400

    Custom AWS scalars for AWSDate, AWSTime, AWSDateTime, AWSEmail, AWSPhone, and AWSURL

--- packages/appsync-emulator-serverless/schemaWrapper.js
+++ packages/appsync-emulator-serverless/schemaWrapper.js
@@ -1,7 +1,25 @@
 const GraphQLJSON = require('graphql-type-json');
 
+const {
+  GraphQLDate,
+  GraphQLTime,
+  GraphQLDateTime
+} = require('graphql-iso-date');
+
+const {
+  EmailAddress,
+  PhoneNumber,
+  URL
+} = require('@okgrow/graphql-scalars');
+
 const scalars = {
   AWSJSON: GraphQLJSON,
+  AWSDate: GraphQLDate,
+  AWSTime: GraphQLTime,
+  AWSDateTime: GraphQLDateTime,
+  AWSEmail: EmailAddress,
+  AWSPhone: PhoneNumber,
+  AWSURL: URL
 };
 
 const wrapSchema = schemaString => {
