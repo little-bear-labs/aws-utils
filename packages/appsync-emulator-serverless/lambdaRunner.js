5aff0e1bb0199abe80c9f0f206f8284c74762b5c
pueue <dev@pueue.com>
2018-11-21 10:31:55 +0900

    feat(lambdaRunner): Allow a lambda async

--- packages/appsync-emulator-serverless/lambdaRunner.js
+++ packages/appsync-emulator-serverless/lambdaRunner.js
@@ -1,35 +1,38 @@
+const { promisify } = require('util');
 const log = require('logdown')('appsync-emulator:lambdaRunner');
+const { create: createUtils } = require('./util');
 
+const sendOutput = output => {
+  process.send({ type: 'success', output }, process.exit);
+}
 const sendErr = err => {
-  process.send({ type: 'error', error: err }, () => {
-    process.exit();
-  });
+  process.send({ type: 'error', error: err }, process.exit);
 };
 
-process.once('message', ({ module, handlerPath, handlerMethod, payload }) => {
+const run = async ({ lambda, context, payload }) => {
+  if (!createUtils().isAsync(lambda)) {
+    lambda = promisify(lambda);
+  }
+  const output = await lambda(payload, context);
+  return output;
+}
+
+process.once('message', async ({ module, handlerPath, handlerMethod, payload }) => {
   try {
     log.info('load', module);
     // eslint-disable-next-line
     const handlerModule = require(module);
     if (!handlerModule[handlerMethod]) {
-      sendErr(
-        new Error(
-          `Module : ${handlerPath} does not have export: ${handlerMethod}`,
-        ),
+      throw new Error(
+        `Module : ${handlerPath} does not have export: ${handlerMethod}`,
       );
     }
 
     log.info('invoke', handlerMethod);
+    const lambda = handlerModule[handlerMethod];
     const context = {};
-    handlerModule[handlerMethod](payload, context, (err, output) => {
-      if (err) {
-        sendErr(err);
-        return;
-      }
-      process.send({ type: 'success', output }, () => {
-        process.exit();
-      });
-    });
+    const output = await run({ lambda, context, payload });
+    sendOutput(output);
   } catch (err) {
     sendErr(err);
   }
@@ -43,3 +46,7 @@ process.on('unhandledRejection', err => {
   log.error('unhandledRejection in lambda', err);
   process.exit(1);
 });
+
+module.exports = {
+  run,
+};
