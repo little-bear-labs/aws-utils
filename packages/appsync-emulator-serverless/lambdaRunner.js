5a1483ab022106db2a629e4443453037b16d4ed6
pueue <dev@pueue.com>
2018-11-21 17:06:45 +0900

    style: Format code with prettier

--- packages/appsync-emulator-serverless/lambdaRunner.js
+++ packages/appsync-emulator-serverless/lambdaRunner.js
@@ -1,44 +1,50 @@
-const { promisify } = require('util');
 const log = require('logdown')('appsync-emulator:lambdaRunner');
 const { create: createUtils } = require('./util');
 
 const sendOutput = output => {
   process.send({ type: 'success', output }, process.exit);
-}
+};
 const sendErr = err => {
   process.send({ type: 'error', error: err }, process.exit);
 };
 
 const run = async ({ lambda, context, payload }, callback) => {
-  const output = lambda(payload, context, callback)
+  const output = lambda(payload, context, callback);
 
-  if (createUtils().isPromise(output)) {
-    return await output;
-  }
-}
+  if (!createUtils().isPromise(output)) return;
+  const promiseOutput = await output;
+  return promiseOutput;
+};
 
-process.once('message', async ({ module, handlerPath, handlerMethod, payload }) => {
-  try {
-    log.info('load', module);
-    // eslint-disable-next-line
-    const handlerModule = require(module);
-    if (!handlerModule[handlerMethod]) {
-      throw new Error(
-        `Module : ${handlerPath} does not have export: ${handlerMethod}`,
+process.once(
+  'message',
+  async ({ module, handlerPath, handlerMethod, payload }) => {
+    try {
+      log.info('load', module);
+      // eslint-disable-next-line
+      const handlerModule = require(module);
+      if (!handlerModule[handlerMethod]) {
+        throw new Error(
+          `Module : ${handlerPath} does not have export: ${handlerMethod}`,
+        );
+      }
+
+      log.info('invoke', handlerMethod);
+      const lambda = handlerModule[handlerMethod];
+      const context = {};
+      const promiseOutput = run(
+        { lambda, context, payload },
+        (err, callbackOutput) => {
+          if (err) sendErr(err);
+          sendOutput(callbackOutput);
+        },
       );
+      sendOutput(await promiseOutput);
+    } catch (err) {
+      sendErr(err);
     }
-
-    log.info('invoke', handlerMethod);
-    const lambda = handlerModule[handlerMethod];
-    const context = {};
-    const output = await run({ lambda, context, payload }, (err, output) => {
-      err ? sendErr(err) : sendOutput(output)
-    });
-    sendOutput(output);
-  } catch (err) {
-    sendErr(err);
-  }
-});
+  },
+);
 
 process.on('uncaughtException', err => {
   log.error('uncaughtException in lambda', err);
