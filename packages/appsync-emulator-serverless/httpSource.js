7150d084d57e0ee882f082c6701f24ba5c1ef50a
Samuel Oltz <soltz@rapiddg.com>
2018-11-26 14:15:30 -0500

    dont assume URLSearchParams is global since it is only global in node > 10

--- packages/appsync-emulator-serverless/httpSource.js
+++ packages/appsync-emulator-serverless/httpSource.js
@@ -1,8 +1,12 @@
+const BackwardCompatibleURLSearchParams = require('url').URLSearchParams;
 const fetch = require('node-fetch');
 
 const httpSource = async (endpoint, { resourcePath, method, params }) => {
   const { query } = params || {};
-  const queryPath = query === undefined ? '' : `?${new URLSearchParams(query)}`;
+  const queryPath =
+    query === undefined
+      ? ''
+      : `?${new BackwardCompatibleURLSearchParams(query)}`;
 
   const response = await fetch(endpoint + resourcePath + queryPath, {
     ...params,
