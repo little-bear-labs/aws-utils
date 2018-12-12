c49efd12fa7e76f039e297892b61eee36c150fb0
Daniela Bento <daniela.bento@olx.com>
2018-12-12 10:52:37 +0000

    Add Python compatible Lambda Runner
    Python Lambda Runner uses the child_process module to spawn the sls
    process. This code is based on serverless-offline-python. It receives
    the serverless directory, the handler path, the handler method and the
    payload. The serverless directory will indicate where the sls command
    should be invoked. In this case the handler function is configured like
    the following example:
    
    ```
    functions:
      example:
        handler: example/handler.example
        timeout: 10
        runtime: python3.6
    ```
    
    The environment is inherit from the main process.

---
+++ packages/appsync-emulator-serverless/lambdaRunnerPython.js
@@ -0,0 +1,69 @@
+const log = require('logdown')('appsync-emulator:lambdaRunner');
+
+const parseErrorStack = error =>
+  error.stack
+    .replace(/at /g, '')
+    .split('\n    ')
+    .slice(1);
+
+const sendOutput = output => {
+  process.send({ type: 'success', output }, process.exit);
+};
+const sendErr = err => {
+  let error;
+  if (err instanceof Error) {
+    error = {
+      stackTrace: parseErrorStack(err),
+      errorType: err.constructor.name,
+      errorMessage: err.message,
+    };
+  } else {
+    error = err;
+  }
+  process.send({ type: 'error', error }, process.exit);
+};
+
+process.once(
+  'message',
+  async ({ serverlessDirectory, handlerPath, handlerMethod, payload}) => {
+    try {
+        log.info('load', module);
+   
+        var spawn = require('child_process').spawn          
+        var args = ["invoke", "local", "-f", handlerMethod]
+
+        var sls = spawn("sls", 
+            args, 
+            {
+                env: process.env,
+                stdio: ['pipe', null, 'pipe'],
+                shell: '/bin/bash',
+                cwd: serverlessDirectory
+            });
+
+        sls.stdin.write(JSON.stringify(payload) + "\n");
+        sls.stdin.end();
+        
+        let results = ''
+        sls.stdout.on('data', function(data) {
+            results = data.toString('utf8');
+            results = results.replace('\n', '')
+        })
+
+        sls.on('close', function(code) {
+            sendOutput(results);
+        });
+    } catch (err) {
+      sendErr(err);
+    }
+  },
+);
+
+process.on('uncaughtException', err => {
+  log.error('uncaughtException in lambda', err);
+  process.exit(1);
+});
+process.on('unhandledRejection', err => {
+  log.error('unhandledRejection in lambda', err);
+  process.exit(1);
+});
