a2c55bd178758cd5daecfb29adb5bb9fe14da73b
Matt Gibson <downrightlies@gmail.com>
2019-02-17 20:50:37 +0000

    Add larger timeouts to allow slow specs to pass on Mac

--- packages/appsync-emulator-serverless/__test__/tester.test.js
+++ packages/appsync-emulator-serverless/__test__/tester.test.js
@@ -8,7 +8,8 @@ const { AWSAppSyncClient } = require('aws-appsync');
 // eslint-disable-next-line
 global.fetch = require('node-fetch');
 
-describe('appasync-emulator-serverless/tester', () => {
+// These specs execute slowly on Mac, so need a larger timeout.
+describe('appsync-emulator-serverless/tester', () => {
   let serverSetup;
   beforeEach(async () => {
     jest.setTimeout(60 * 1000);
@@ -64,57 +65,61 @@ describe('appasync-emulator-serverless/tester', () => {
     });
   });
 
-  it('supports localstack', async () => {
-    serverSetup = await createTestServer({
-      serverless: `${__dirname}/example/serverless.yml`,
-      dynamodbConfig: {
-        endpoint: 'http://localhost:61023',
-        accessKeyId: 'fake',
-        secretAccessKey: 'fake',
-        region: 'fake',
-      },
-    });
-
-    const client = connectTestServer(serverSetup, AWSAppSyncClient);
-
-    const sub = await client.subscribe({
-      query: gql`
-        subscription sub {
-          subscribeToPutQuoteRequest {
-            id
-            commodity
-            amount
-          }
-        }
-      `,
-    });
-    const waiting = new Promise(accept => sub.subscribe(accept));
-    await client.mutate({
-      mutation: gql`
-        mutation test($input: QuoteRequestInput!) {
-          putQuoteRequest(input: $input) {
-            id
-            commodity
-            amount
-          }
-        }
-      `,
-      variables: {
-        input: {
-          commodity: 'foo',
-          amount: 100.5,
+  it(
+    'supports localstack',
+    async () => {
+      serverSetup = await createTestServer({
+        serverless: `${__dirname}/example/serverless.yml`,
+        dynamodbConfig: {
+          endpoint: 'http://localhost:61023',
+          accessKeyId: 'fake',
+          secretAccessKey: 'fake',
+          region: 'fake',
         },
-      },
-    });
+      });
 
-    const event = await waiting;
-    expect(event).toMatchObject({
-      data: {
-        subscribeToPutQuoteRequest: {
-          amount: 100.5,
-          commodity: 'foo',
+      const client = connectTestServer(serverSetup, AWSAppSyncClient);
+
+      const sub = await client.subscribe({
+        query: gql`
+          subscription sub {
+            subscribeToPutQuoteRequest {
+              id
+              commodity
+              amount
+            }
+          }
+        `,
+      });
+      const waiting = new Promise(accept => sub.subscribe(accept));
+      await client.mutate({
+        mutation: gql`
+          mutation test($input: QuoteRequestInput!) {
+            putQuoteRequest(input: $input) {
+              id
+              commodity
+              amount
+            }
+          }
+        `,
+        variables: {
+          input: {
+            commodity: 'foo',
+            amount: 100.5,
+          },
         },
-      },
-    });
-  });
+      });
+
+      const event = await waiting;
+      expect(event).toMatchObject({
+        data: {
+          subscribeToPutQuoteRequest: {
+            amount: 100.5,
+            commodity: 'foo',
+          },
+        },
+      });
+    },
+    40000,
+  );
 });
