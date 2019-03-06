1302390f13bee9f199988851f4110ac990588c59
cbaron <topherbaron@gmail.com>
2019-03-06 18:35:17 -0500

    vtl context only gets headers from request

--- packages/appsync-emulator-serverless/schema.js
+++ packages/appsync-emulator-serverless/schema.js
@@ -47,14 +47,14 @@ class AppSyncError extends Error {
 const buildVTLContext = ({ root, vars, context, info }, result = null) => {
   const {
     jwt: { iss: issuer, sub },
-    request,
+    request: { headers },
   } = context;
   const util = createUtils();
   const args = javaify(vars);
   const vtlContext = {
     arguments: args,
     args,
-    request,
+    request: { headers },
     identity: javaify({
       sub,
       issuer,
